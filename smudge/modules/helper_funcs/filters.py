1782a15339eaf601bf85c2fb16c01377f1a28140
Renatoh <49354525+RenatohRibeiro@users.noreply.github.com>
2020-01-21 00:49:24 -0300

    [filters] import fixes

--- smudge/modules/helper_funcs/filters.py
+++ smudge/modules/helper_funcs/filters.py
@@ -1,7 +1,7 @@
 from telegram import Message
 from telegram.ext import BaseFilter
 
-from hitsuki import SUPPORT_USERS, SUDO_USERS
+from haruka import SUPPORT_USERS, SUDO_USERS
 
 
 class CustomFilters(object):
