2ba3817c2ae997f5115536bf24a51408843cfd72
Renatoh <49354525+RenatohRibeiro@users.noreply.github.com>
2020-01-21 01:10:56 -0300

    [main] imports fixes

--- smudge/__main__.py
+++ smudge/__main__.py
@@ -39,7 +39,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("hitsuki.modules." + module_name)
+    imported_module = importlib.import_module("smudge.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
