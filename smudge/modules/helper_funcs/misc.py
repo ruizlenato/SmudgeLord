26142b67f8621c468c68a2a007a9699003695c9c
Renatoh <49354525+RenatohRibeiro@users.noreply.github.com>
2020-01-21 00:59:31 -0300

    [misc] imports fixes

--- smudge/modules/helper_funcs/misc.py
+++ smudge/modules/helper_funcs/misc.py
@@ -4,8 +4,8 @@ from typing import List, Dict
 from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode
 from telegram.error import TelegramError
 
-from haruka import LOAD, NO_LOAD
-from haruka.modules.translations.strings import tld
+from smudge import LOAD, NO_LOAD
+from smudge.modules.translations.strings import tld
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
