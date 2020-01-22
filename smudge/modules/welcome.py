aa703d2cb3e572cc13b49947edbdbb660c8935b6
Renatoh <49354525+RenatohRibeiro@users.noreply.github.com>
2020-01-22 16:06:09 -0300

    [Welcome] Fixes

--- smudge/modules/welcome.py
+++ smudge/modules/welcome.py
@@ -3,8 +3,7 @@ import re
 from html import escape
 from typing import Optional, List
 
-from telegram import Message, Chat, Update, Bot, User, CallbackQuery, ChatMember, MessageEntity
-from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import Message, Chat, Update, Bot, User, CallbackQuery, MessageEntity	
 from telegram.error import BadRequest
 from telegram.ext import MessageHandler, Filters, CommandHandler, run_async, CallbackQueryHandler
 from telegram.utils.helpers import mention_markdown, mention_html, escape_markdown
