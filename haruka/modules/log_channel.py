b8f5b485d3f313971694d6763a88431e2f44967c
Akito Mizukito <akito@evolution-x.org>
2020-04-19 00:18:15 +0900

    log_channel: Fixed derp that causes 'link:' to not work
    Change-Id: If96bb46fbcf58963a3ab7afb6c2a69a143957bb8
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- haruka/modules/log_channel.py
+++ haruka/modules/log_channel.py
@@ -25,8 +25,8 @@ def loggable(func):
         if result:
             if chat.type == chat.SUPERGROUP and chat.username:
                 result += tld(
-                    chat.id, "log_channel_link".format(chat.username,
-                                                       message.message_id))
+                    chat.id, "log_channel_link").format(chat.username,
+                                                       message.message_id)
             log_chat = sql.get_chat_log_channel(chat.id)
             if log_chat:
                 send_log(bot, log_chat, chat.id, result)
