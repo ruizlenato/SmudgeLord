d92cdd7fa5e2b1c64b6ec188ba531fa9deaef653
Gegham Zakaryan <zakaryan.2004@outlook.com>
2020-04-29 00:11:15 +0900

    events: Allow !
    Change-Id: I14185dbd2a5f6478b97032733c82f4f6b12c98f6
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- haruka/events.py
+++ haruka/events.py
@@ -23,8 +23,12 @@ def register(**args):
     """ Registers a new message. """
     pattern = args.get('pattern', None)
 
+    r_pattern = r'^[/!]'
+
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
+
+    args['pattern'] = pattern.replace('^/', r_pattern, 1)
 
     def decorator(func):
         tbot.add_event_handler(func, events.NewMessage(**args))
