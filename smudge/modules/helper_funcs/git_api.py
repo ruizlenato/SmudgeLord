78d4baba4a4f56077028877502c1ff2e4244a2ba
Nuno Penim <nuno.penim@gmail.com>
2020-06-06 17:22:42 +0100

    GitHub Module Added

---
+++ smudge/modules/helper_funcs/git_api.py
@@ -0,0 +1,72 @@
+import urllib.request as url
+import json
+
+VERSION = "1.0.2"
+APIURL = "http://api.github.com/repos/"
+
+def vercheck() -> str:
+    return str(VERSION)
+
+def getData(repoURL):
+    try:
+        with url.urlopen(APIURL + repoURL + "/releases") as data_raw:
+            return json.loads(data_raw.read().decode())
+    except Exception:
+        return None
+
+#Release-wise stuff
+
+def getReleaseData(repoData, index):
+    if index < len(repoData):
+        return repoData[index]
+    else:
+        return None
+
+def getAuthor(releaseData):
+    if releaseData is None:
+        return None
+    return releaseData['author']['login']
+    
+def getAuthorUrl(releaseData):
+    if releaseData is None:
+        return None    
+    return releaseData['author']['html_url']
+    
+def getReleaseName(releaseData):
+    if releaseData is None:
+        return None    
+    return releaseData['name']
+
+def getReleaseDate(releaseData):
+    if releaseData is None:
+        return None    
+    return releaseData['published_at']
+
+def getAssetsSize(releaseData):
+    if releaseData is None:
+        return None    
+    return len(releaseData['assets'])
+  
+def getAssets(releaseData):
+    if releaseData is None:
+        return None
+    return releaseData['assets']
+    
+def getBody(releaseData): #changelog stuff
+    if releaseData is None:
+        return None
+    return releaseData['body']
+
+#Asset-wise stuff
+
+def getReleaseFileName(asset): 
+    return asset['name']
+
+def getReleaseFileURL(asset):
+    return asset['browser_download_url']
+
+def getDownloadCount(asset):
+    return asset['download_count']
+
+def getSize(asset):
+    return asset['size']
---
+++ smudge/modules/sql/github_sql.py
@@ -0,0 +1,76 @@
+import threading
+
+from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
+
+from smudge.modules.sql import SESSION, BASE
+
+class GitHub(BASE):
+    __tablename__ = "github"
+    chat_id = Column(String(14), primary_key=True)
+    name = Column(UnicodeText, primary_key=True)
+    value = Column(UnicodeText, nullable=False)
+    backoffset = Column(Integer, nullable=False, default=0)
+
+    def __init__(self, chat_id, name, value, backoffset):
+        self.chat_id = str(chat_id)
+        self.name = name
+        self.value = value
+        self.backoffset = backoffset
+
+    def __repr__(self):
+        return "<Git Repo %s>" % self.name
+
+
+GitHub.__table__.create(checkfirst=True)
+
+GIT_LOCK = threading.RLock()
+
+
+def add_repo_to_db(chat_id, name, value, backoffset):
+    with GIT_LOCK:
+        prev = SESSION.query(GitHub).get((str(chat_id), name))
+        if prev:
+            SESSION.delete(prev)
+        repo = GitHub(str(chat_id), name, value, backoffset)
+        SESSION.add(repo)
+        SESSION.commit()
+
+
+def get_repo(chat_id, name):
+    try:
+        return SESSION.query(GitHub).get((str(chat_id), name))
+    finally:
+        SESSION.close()
+
+
+def rm_repo(chat_id, name):
+    with GIT_LOCK:
+        repo = SESSION.query(GitHub).get((str(chat_id), name))
+        if repo:
+            SESSION.delete(repo)
+            SESSION.commit()
+            return True
+        else:
+            SESSION.close()
+            return False
+
+
+def get_all_repos(chat_id):
+    try:
+        return SESSION.query(GitHub).filter(GitHub.chat_id == str(chat_id)).order_by(GitHub.name.asc()).all()
+    finally:
+        SESSION.close()
+
+
+def num_repos():
+    try:
+        return SESSION.query(GitHub).count()
+    finally:
+        SESSION.close()
+
+
+def num_chats():
+    try:
+        return SESSION.query(func.count(distinct(GitHub.chat_id))).scalar()
+    finally:
+        SESSION.close()
