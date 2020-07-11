f7fc69bcb351817a2f26c69e4e215b6d3da76be7
renatohribeiro <renatohribeh@outlook.com>
2020-07-11 20:44:19 +0000

    removal of broken modules/commands

--- haruka/modules/memes.py
+++ haruka/modules/memes.py
@@ -4,7 +4,6 @@ from io import BytesIO
 import base64
 from spongemock import spongemock
 from zalgo_text import zalgo
-from deeppyer import deepfry
 import os
 from pathlib import Path
 import glob
@@ -117,44 +116,6 @@ def vapor(bot: Bot, update: Update, args: List[str]):
 
 
 @run_async
-def mafiatext(bot: Bot, update: Update, args: List[str]):
-    message = update.effective_message
-    chat = update.effective_chat  # type: Optional[Chat]
-
-    noreply = False
-    if message.reply_to_message:
-        data = message.reply_to_message.text
-    elif args:
-        noreply = True
-        data = message.text.split(None, 1)[1]
-    else:
-        noreply = True
-        data = tld(chat.id, "memes_no_message")
-
-    if not Path('mafia.jpg').is_file():
-        LOGGER.warning(
-            "mafia.jpg not found! Mafia memes module is turned off!")
-        return
-
-    for mocked in glob.glob("mocked*"):
-        os.remove(mocked)
-    reply_text = spongemock.mock(data)
-
-    randint = random.randint(1, 699)
-    magick = """convert mafia.jpg -font Impact -pointsize 50 -size 1280x720 -stroke white -strokewidth 1 -fill black -background none -gravity north caption:"{}" -flatten mafiaed{}.jpg""".format(
-        reply_text, randint)
-    os.system(magick)
-    with open('mafiaed{}.jpg'.format(randint), 'rb') as mockedphoto:
-        if noreply:
-            message.reply_photo(photo=mockedphoto,
-                                reply=message.reply_to_message)
-        else:
-            message.reply_to_message.reply_photo(
-                photo=mockedphoto, reply=message.reply_to_message)
-    os.remove('mafiaed{}.jpg'.format(randint))
-
-
-@run_async
 def pidortext(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
     chat = update.effective_chat  # type: Optional[Chat]
@@ -188,80 +149,6 @@ def pidortext(bot: Bot, update: Update, args: List[str]):
             message.reply_to_message.reply_photo(
                 photo=mockedphoto, reply=message.reply_to_message)
     os.remove('4pdaed{}.jpg'.format(randint))
-
-
-@run_async
-def kimtext(bot: Bot, update: Update, args: List[str]):
-    message = update.effective_message
-    chat = update.effective_chat  # type: Optional[Chat]
-
-    noreply = False
-    if message.reply_to_message:
-        data = message.reply_to_message.text
-    elif args:
-        noreply = True
-        data = message.text.split(None, 1)[1]
-    else:
-        noreply = True
-        data = tld(chat.id, "memes_no_message")
-
-    if not Path('kim.jpg').is_file():
-        LOGGER.warning("kim.jpg not found! Kim memes module is turned off!")
-        return
-    for mocked in glob.glob("mocked*"):
-        os.remove(mocked)
-    reply_text = spongemock.mock(data)
-
-    randint = random.randint(1, 699)
-    magick = """convert kim.jpg -font Impact -pointsize 50 -size 480x360 -stroke black -strokewidth 1 -fill white -background none -gravity north caption:"{}" -flatten kimed{}.jpg""".format(
-        reply_text, randint)
-    os.system(magick)
-    with open('kimed{}.jpg'.format(randint), 'rb') as mockedphoto:
-        if noreply:
-            message.reply_photo(photo=mockedphoto,
-                                reply=message.reply_to_message)
-        else:
-            message.reply_to_message.reply_photo(
-                photo=mockedphoto, reply=message.reply_to_message)
-    os.remove('kimed{}.jpg'.format(randint))
-
-
-@run_async
-def hitlertext(bot: Bot, update: Update, args: List[str]):
-    message = update.effective_message
-    chat = update.effective_chat  # type: Optional[Chat]
-
-    noreply = False
-    if message.reply_to_message:
-        data = message.reply_to_message.text
-    elif args:
-        noreply = True
-        data = message.text.split(None, 1)[1]
-    else:
-        noreply = True
-        data = tld(chat.id, "memes_no_message")
-
-    if not Path('hitler.jpg').is_file():
-        LOGGER.warning(
-            "hitler.jpg not found! Hitler memes module is turned off!")
-        return
-    for mocked in glob.glob("hitlered*"):
-        os.remove(mocked)
-    reply_text = spongemock.mock(data)
-
-    randint = random.randint(1, 699)
-    magick = """convert hitler.jpg -font Impact -pointsize 50 -size 615x409 -stroke black -strokewidth 1 -fill white -background none -gravity north caption:"{}" -flatten hitlered{}.jpg""".format(
-        reply_text, randint)
-    os.system(magick)
-    with open('hitlered{}.jpg'.format(randint), 'rb') as mockedphoto:
-        if noreply:
-            message.reply_photo(photo=mockedphoto,
-                                reply=message.reply_to_message)
-        else:
-            message.reply_to_message.reply_photo(
-                photo=mockedphoto, reply=message.reply_to_message)
-    os.remove('hitlered{}.jpg'.format(randint))
-
 
 @run_async
 def spongemocktext(bot: Bot, update: Update, args: List[str]):
@@ -320,59 +207,6 @@ def zalgotext(bot: Bot, update: Update, args: List[str]):
         message.reply_text(reply_text)
     else:
         message.reply_to_message.reply_text(reply_text)
-
-
-# Less D A N K modules by @skittles9823 # holi fugg I did some maymays ^^^
-# shitty maymay modules made by @divadsn vvv
-
-
-@run_async
-def deepfryer(bot: Bot, update: Update):
-    message = update.effective_message
-    chat = update.effective_chat  # type: Optional[Chat]
-    if message.reply_to_message:
-        data = message.reply_to_message.photo
-        data2 = message.reply_to_message.sticker
-    else:
-        data = []
-        data2 = []
-
-    # check if message does contain media and cancel when not
-    if not data and not data2:
-        message.reply_text("What am I supposed to do with this?!")
-        return
-
-    # download last photo (highres) as byte array
-    if data:
-        photodata = data[len(data) - 1].get_file().download_as_bytearray()
-        image = Image.open(io.BytesIO(photodata))
-    elif data2:
-        sticker = context.bot.get_file(data2.file_id)
-        sticker.download('sticker.png')
-        image = Image.open("sticker.png")
-
-    # the following needs to be executed async (because dumb lib)
-    #bot = context.bot
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(process_deepfry(image, message.reply_to_message, bot, update))
-    loop.close()
-
-
-async def process_deepfry(image: Image, reply: Message, bot: Bot, update: Update):
-    image = await deepfry(img=image, token=DEEPFRY_TOKEN, url_base='westeurope')
-    if image is None:
-        update.effective_message.reply_text("There has been an unspecified failure")
-        return
-    bio = BytesIO()
-    bio.name = 'image.jpeg'
-    image.save(bio, 'JPEG')
-
-    # send it back
-    bio.seek(0)
-    reply.reply_photo(bio)
-    if Path("sticker.png").is_file():
-        os.remove("sticker.png")
-
 
 @run_async
 def shout(bot: Bot, update: Update, args: List[str]):
@@ -479,12 +313,8 @@ VAPOR_HANDLER = DisableAbleCommandHandler("vapor",
                                           pass_args=True,
                                           admin_ok=True)
 MOCK_HANDLER = DisableAbleCommandHandler("mock", spongemocktext, admin_ok=True, pass_args=True)
-KIM_HANDLER = DisableAbleCommandHandler("kim", kimtext, admin_ok=True, pass_args=True)
-MAFIA_HANDLER = DisableAbleCommandHandler("mafia", mafiatext, admin_ok=True, pass_args=True)
 PIDOR_HANDLER = DisableAbleCommandHandler("pidor", pidortext, admin_ok=True, pass_args=True)
-HITLER_HANDLER = DisableAbleCommandHandler("hitler", hitlertext, admin_ok=True, pass_args=True)
 ZALGO_HANDLER = DisableAbleCommandHandler("zalgofy", zalgotext, pass_args=True)
-DEEPFRY_HANDLER = DisableAbleCommandHandler("deepfry", deepfryer, admin_ok=True)
 SHOUT_HANDLER = DisableAbleCommandHandler("shout", shout, pass_args=True)
 INSULTS_HANDLER = DisableAbleCommandHandler("insults", insults, admin_ok=True)
 RUNS_HANDLER = DisableAbleCommandHandler("runs", runs, admin_ok=True)
@@ -493,7 +323,6 @@ SLAP_HANDLER = DisableAbleCommandHandler("slap",
                                          pass_args=True,
                                          admin_ok=True)
 
-dispatcher.add_handler(MAFIA_HANDLER)
 dispatcher.add_handler(PIDOR_HANDLER)
 dispatcher.add_handler(SHOUT_HANDLER)
 dispatcher.add_handler(OWO_HANDLER)
@@ -501,9 +330,6 @@ dispatcher.add_handler(STRETCH_HANDLER)
 dispatcher.add_handler(VAPOR_HANDLER)
 dispatcher.add_handler(MOCK_HANDLER)
 dispatcher.add_handler(ZALGO_HANDLER)
-dispatcher.add_handler(DEEPFRY_HANDLER)
-dispatcher.add_handler(KIM_HANDLER)
-dispatcher.add_handler(HITLER_HANDLER)
 dispatcher.add_handler(INSULTS_HANDLER)
 dispatcher.add_handler(RUNS_HANDLER)
 dispatcher.add_handler(SLAP_HANDLER)
