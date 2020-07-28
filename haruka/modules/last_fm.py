e61887bfecb407cd2ce0d1a6ab9e248c51a6487a
renatohribeiro <renatohribeh@outlook.com>
2020-07-28 17:14:09 -0300

    6Test LastFM

--- haruka/modules/last_fm.py
+++ haruka/modules/last_fm.py
@@ -64,7 +64,7 @@ def last_fm(bot: Bot, update: Update):
         artist = first_track.get("artist").get("name")
         song = first_track.get("name")
         loved = int(first_track.get("loved"))
-        rep = tld(chat.id, "lastfm_listening").format(scrobbles)
+        rep = tld(chat.id, "lastfm_listening").format
         if not loved:
             rep += tld(chat.id, "lastfm_scrb").format(artist, song)
         else:
