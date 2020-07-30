60ec068835d051cbc5258661163af733fe10130e
renatohribeiro <renatohribeh@outlook.com>
2020-07-30 13:21:51 -0300

    Undoing the shit

--- haruka/modules/translations/strings.py
+++ haruka/modules/translations/strings.py
@@ -1,32 +1,18 @@
-#    Smudge (A telegram bot project)
-#    Copyright (C) 2017-2019 Paul Larsen
-#    Copyright (C) 2019-2020 Akito Mizukito (Haruka Network Development)
-
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+import xml.etree.ElementTree as et
+import os
 import yaml
 from codecs import encode, decode
 
 from haruka import LOGGER
-from haruka.modules.sql.locales_sql import prev_locale
+from haruka.modules.sql.translation import prev_locale
 
-LANGUAGES = ['en', 'pt-BR']
+LANGUAGES = ['en', 'pt-br']
 
 strings = {}
 
 for i in LANGUAGES:
-    strings[i] = yaml.full_load(open("locales/" + i + ".yml", "r"))
+    strings[i] = yaml.full_load(open(os.path.dirname(__file__) + f"/{i}/string.yml", "r"))
+    print(f"Loaded {i}/string.yml")
 
 
 def tld(chat_id, t, show_none=True):
@@ -35,23 +21,20 @@ def tld(chat_id, t, show_none=True):
     if LANGUAGE:
         LOCALE = LANGUAGE.locale_name
         if LOCALE in ('en') and t in strings['en']:
-            result = decode(
-                encode(strings['en'][t], 'latin-1', 'backslashreplace'),
-                'unicode-escape')
+            result = decode(encode(strings['en'][t], 'latin-1', 'backslashreplace'),
+                            'unicode-escape')
             return result
-        elif LOCALE in ('pt') and t in strings['pt-BR']:
-            result = decode(
-                encode(strings['pt-BR'][t], 'latin-1', 'backslashreplace'),
-                'unicode-escape')
+        elif LOCALE in ('pt-br') and t in strings['pt-br']:
+            result = decode(encode(strings['pt-br'][t], 'latin-1', 'backslashreplace'),
+                            'unicode-escape')
             return result
 
     if t in strings['en']:
-        result = decode(
-            encode(strings['en'][t], 'latin-1', 'backslashreplace'),
-            'unicode-escape')
+        result = decode(encode(strings['en'][t], 'latin-1', 'backslashreplace'),
+                        'unicode-escape')
         return result
 
-    err = f"No string found for {t}.\nReport it to @Renatoh."
+    err = f"No string found for {t}.\nReport it in @Renatoh."
     LOGGER.warning(err)
     return err
 
@@ -63,11 +46,36 @@ def tld_list(chat_id, t):
         LOCALE = LANGUAGE.locale_name
         if LOCALE in ('en') and t in strings['en']:
             return strings['en'][t]
-        elif LOCALE in ('pt-BR') and t in strings['pt-BR']:
-            return strings['pt-BR'][t]
+        elif LOCALE in ('pt-br') and t in strings['pt-br']:
+            return strings['pt-br'][t]
 
     if t in strings['en']:
         return strings['en'][t]
 
     LOGGER.warning(f"#NOSTR No string found for {t}.")
-    return f"No string found for {t}.\nReport it to @Renatoh."
+    return f"No string found for {t}.\nReport it in @Renatoh."
+
+# def tld_help(chat_id, t):
+#     LANGUAGE = prev_locale(chat_id)
+#     print("tld_help ", chat_id, t)
+#     if LANGUAGE:
+#         LOCALE = LANGUAGE.locale_name
+
+#         t = t + "_help"
+
+#         print("Test2", t)
+
+#         if LOCALE in ('ru') and t in RussianStrings:
+#             return RussianStrings[t]
+#         elif LOCALE in ('ua') and t in UkrainianStrings:
+#             return UkrainianStrings[t]
+#         elif LOCALE in ('es') and t in SpanishStrings:
+#             return SpanishStrings[t]
+#         elif LOCALE in ('tr') and t in TurkishStrings:
+#             return TurkishStrings[t]
+#         elif LOCALE in ('id') and t in IndonesianStrings:
+#             return IndonesianStrings[t]
+#         else:
+#             return False
+#     else:
+#         return False
