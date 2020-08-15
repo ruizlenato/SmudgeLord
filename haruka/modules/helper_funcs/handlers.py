a74d701cd2a2d1c7dc0e21653c93c5fc2f60c288
RenatohRibeiro <renatohribeh@outlook.com>
2020-08-14 21:38:49 -0300

    now i think everything is ready2

--- haruka/modules/helper_funcs/handlers.py
+++ haruka/modules/helper_funcs/handlers.py
@@ -16,6 +16,10 @@ class CustomCommandHandler(tg.CommandHandler):
             (update.message or update.edited_message and self.allow_edited)):
             message = update.message or update.edited_message
 
+            if update.effective_user and int(
+                    update.effective_user.id) == 777000:
+                return False
+
             if message.text and len(message.text) > 1:
                 fst_word = message.text_html.split(None, 1)[0]
                 if len(fst_word) > 1 and any(
