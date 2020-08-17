4978366270779a834ec43e72868ad2e60e5b2be5
RenatohRibeiro <renatohribeh@outlook.com>
2020-08-17 18:34:51 -0300

    [FIX] /stats

--- haruka/modules/antispam.py
+++ haruka/modules/antispam.py
@@ -354,7 +354,7 @@ def clear_gbans(bot: Bot, update: Update):
 
 
 def __stats__():
-    return "• `{}` gbanned users. [Note, We regularly clean off deleted account from the database]".format(
+    return "• `{}` gbanned users [We regularly clean off deleted account from the database].".format(
         sql.num_gbanned_users())
 
 
