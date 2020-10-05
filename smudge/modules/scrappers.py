dabbab51b3d99fb47bf7c5535ad44796afcfc13f
renatohribeiro <renatohribeh@outlook.com>
2020-10-05 02:26:08 -0300

    Fix Audio Download [YTDownloader]

--- smudge/modules/scrappers.py
+++ smudge/modules/scrappers.py
@@ -11,9 +11,6 @@ LOGGER.info("YTDownloader: By @Nick80835 (modified by @Renatoh on Telegram)")
 
 @register(pattern=r"^/yt(?: |)([\S]*)(?: |)([\s\S]*)")
 async def youtube_cmd(event):
-    message = event.pattern_match.group(2)
-    y = message
-    y = y.split()
     youtube_link = event.pattern_match.group(1)
     video = pafy.new(youtube_link)
     video_stream = video.getbest()
@@ -23,9 +20,10 @@ async def youtube_cmd(event):
         await event.reply(f"`Download failed: `[URL]({video_stream.url})")
 
 
-@register(pattern=r"^/(yta|youtubeaudio)(?: |$)(\S*)")
+@register(pattern=r"^/yta(?: |)([\S]*)(?: |)([\s\S]*)")
 async def youtube_audio_cmd(event):
-    video = pafy.new(event.args)
+    youtube_link = event.pattern_match.group(1)
+    video = pafy.new(youtube_link)
     video_stream = video.getbestaudio()
     try:
         await event.client.send_file(event.chat_id, file=video_stream.url)
