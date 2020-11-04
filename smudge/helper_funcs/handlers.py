4f8e4317c2b0658d1000d8cf9f57fb97a73af2e2
DragSama <49992845+DragSama@users.noreply.github.com>
2020-11-04 17:58:11 +0530

    Add spam checker

--- smudge/helper_funcs/handlers.py
+++ smudge/helper_funcs/handlers.py
@@ -1,19 +1,58 @@
 import telegram.ext as tg
 from telegram import Update
-from smudge import ALLOW_EXCL
+from smudge import SUDO_USERS
+from pyrate_limiter import BucketFullException, Duration, RequestRate, Limiter, MemoryListBucket
 
 CMD_STARTERS = ('/', '!')
+
+
+class AntiSpam:
+
+    def __init__(self):
+        self.whitelist = (SUDO_USERS or []) #Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
+        Duration.CUSTOM = 15  # Custom duration, 15 seconds
+        self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
+        self.min_limit = RequestRate(20, Duration.MINUTE)  # 20 / Per minute
+        self.hour_limit = RequestRate(100, Duration.HOUR)  # 100 / Per hour
+        self.daily_limit = RequestRate(1000, Duration.DAY)  # 1000 / Per day
+        self.limiter = Limiter(
+            self.sec_limit,
+            self.min_limit,
+            self.hour_limit,
+            self.daily_limit,
+            bucket_class=MemoryListBucket)
+
+    def check_user(self, user):
+        """
+        Return True if user is to be ignored else False
+        """
+        if user in self.whitelist:
+            return False
+        try:
+            self.limiter.try_acquire(user)
+            return False
+        except BucketFullException:
+            return True
+
+
+SpamChecker = AntiSpam()
+MessageHandlerChecker = AntiSpam()
 
 
 class CustomCommandHandler(tg.CommandHandler):
     def __init__(self, command, callback, run_async=True, **kwargs):
         if "admin_ok" in kwargs:
             del kwargs["admin_ok"]
-        super().__init__(command, callback, run_async=true, **kwargs)
+        super().__init__(command, callback, run_async=run_async, **kwargs)
 
     def check_update(self, update):
         if isinstance(update, Update) and update.effective_message:
             message = update.effective_message
+
+            try:
+                user_id = update.effective_user.id
+            except:
+                user_id = None
 
             if message.text and len(message.text) > 1:
                 fst_word = message.text.split(None, 1)[0]
@@ -32,12 +71,11 @@ class CustomCommandHandler(tg.CommandHandler):
                     ):
                         return None
 
+                    if SpamChecker.check_user(user_id):
+                        return None
+
                     filter_result = self.filters(update)
                     if filter_result:
                         return args, filter_result
                     else:
                         return False
-
-class CustomRegexHandler(tg.MessageHandler):
-    def __init__(self, pattern, callback, friendly="", **kwargs):
-        super().__init__(tg.Filters.regex(pattern), callback, **kwargs)
