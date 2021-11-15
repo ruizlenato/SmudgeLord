970ac294a5c0d65ea720916cbe36c5a01c8dddb2
RenatohRibeiro <renatohribeh@outlook.com>
2021-11-15 13:43:36 -0300

    Added sudoers module
    Co-Authored-By: AndrielFR <andrielkogama2@gmail.com>

---
+++ smudge/plugins/sudoers.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from smudge.database import groups
+from smudge.config import SUDOERS
+
+
+@Client.on_message(filters.command("(broadcast|announcement)") & filters.user(SUDOERS))
+async def broadcast(c: Client, m: Message):
+    sm = await m.reply_text("Broadcasting...")
+    command = m.text.split()[0]
+    text = m.text[len(command) + 1 :]
+    chats = await groups.all()
+    success = []
+    fail = []
+    for chat in chats:
+        try:
+            if await c.send_message(chat.id, text):
+                success.append(chat.id)
+            else:
+                fail.append(chat.id)
+        except:
+            fail.append(chat.id)
+    await sm.edit_text(
+        f"Anúncio feito com sucesso! Sua mensagem foi enviada em um total de <code>{len(success)}</code> grupos e falhou o envio em <code>{len(fail)}</code> grupos."
+    )
+
+
+@Client.on_message(filters.command("restart") & filters.user(SUDOERS))
+async def broadcast(c: Client, m: Message):
+    await m.reply_text("Restarting...")
+    args = [sys.executable, "-m", "smudge"]
+    os.execl(sys.executable, *args)
