11c6d60e4f26205fef33acdde22273a294584770
ruizlenato <renatohribeh@outlook.com>
2022-02-02 22:24:31 -0300

    Misc: /cep added to fetch information from a Brazilian zip code

--- smudge/plugins/misc.py
+++ smudge/plugins/misc.py
@@ -466,5 +466,28 @@ async def ytdl(c: Client, m: Message):
     shutil.rmtree(tempdir, ignore_errors=True)
 
 
+@Client.on_message(filters.command(["cep"], prefixes="/"))
+async def lastfm(c: Client, m: Message):
+    try:
+        cep = m.text.split(maxsplit=1)[1]
+    except IndexError:
+        await m.reply_text(await tld(m.chat.id, "no_cep"))
+        return
+
+    base_url = "https://brasilapi.com.br/api/cep/v1"
+    res = await http.get(f"{base_url}/{cep}")
+    city = res.json().get("city")
+    state = res.json().get("state")
+    neighborhood = res.json().get("neighborhood")
+    street = res.json().get("street")
+
+    if res.status_code == 404:
+        await m.reply_text((await tld(m.chat.id, "cep_error")))
+        return
+    else:
+        rep = (await tld(m.chat.id, "cep_strings")).format(
+            cep, city, state, neighborhood, street
+        )
+        await m.reply_text(rep)
 plugin_name = "misc_name"
 plugin_help = "misc_help"
