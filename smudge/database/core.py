4de34728e9e9364b49d8a73661e48ef3bde19cd0
RuizLenato <ruizlenato@protonmail.com>
2022-02-21 22:23:27 -0300

    treewide: update

--- smudge/database/core.py
+++ smudge/database/core.py
@@ -9,7 +9,6 @@ from tortoise.exceptions import DoesNotExist, IntegrityError
 class users(Model):
     id = fields.IntField(pk=True)
     lastfm_username = fields.TextField(null=True)
-    afk_reason = fields.TextField(default="False")
 
 
 class groups(Model):
