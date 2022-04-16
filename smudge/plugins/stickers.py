f33ffca20246943205f7c8823ba791a040bf8a30
ruizlenato <ruizlenato@protonmail.com>
2022-04-16 20:06:01 -0300

    Stickers: Added support for video stickers in /getsticker

--- smudge/plugins/stickers.py
+++ smudge/plugins/stickers.py
@@ -33,7 +33,19 @@ async def getsticker(c: Client, m: Message):
     try:
         sticker = m.reply_to_message.sticker
         if sticker.is_animated or sticker.is_video:
-            await m.reply_text(await tld(m, "Stickers.animated_not_supported"))
+            with tempfile.TemporaryDirectory() as tempdir:
+                path = os.path.join(tempdir, "getsticker")
+            sticker_file = await c.download_media(
+                message=m.reply_to_message,
+                file_name=f"{path}/{sticker.set_name}.gif",
+            )
+            await m.reply_to_message.reply_document(
+                document=sticker_file,
+                caption=(await tld(m, "Stickers.info_sticker")).format(
+                    sticker.emoji, sticker.file_id
+                ),
+            ),
+            shutil.rmtree(tempdir, ignore_errors=True)
         elif not sticker.is_video and not sticker.is_animated:
             with tempfile.TemporaryDirectory() as tempdir:
                 path = os.path.join(tempdir, "getsticker")
