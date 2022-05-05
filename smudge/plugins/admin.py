9880131fc328fa5c961868efd5ab3cf21787d3a8
ruizlenato <ruizlenato@protonmail.com>
2022-05-05 19:25:30 -0300

    admin: fixes based on Pyrogram 2.0

--- smudge/plugins/admin.py
+++ smudge/plugins/admin.py
@@ -3,7 +3,7 @@
 import asyncio
 from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.enums import ChatType, ChatMemberStatus
+from pyrogram.enums import ChatType, ChatMemberStatus, ChatMembersFilter
 from pyrogram.errors import BadRequest, Forbidden, FloodWait
 
 from smudge import Smudge
@@ -30,13 +30,13 @@ async def cleanup(c: Smudge, m: Message):
         else:
             return await m.reply_text(await tld(m, "Admin.noadmin"))
     deleted_users = []
-    sent = await m.reply_text(await tld(m, "Admin.cleanup_start"))
-    async for a in c.iter_chat_members(chat_id=m.chat.id, filter="all"):
+    mes = await m.reply_text(await tld(m, "Admin.cleanup_start"))
+    async for a in c.get_chat_members(chat_id=m.chat.id):
         if a.user.is_deleted:
             try:
                 await c.ban_chat_member(m.chat.id, a.user.id)
                 deleted_users.append(a)
-                await sent.edit_text(await tld(m, "Admin.cleanup_start_d")).format(
+                await mes.edit_text(await tld(m, "Admin.cleanup_start_d")).format(
                     {len(deleted_users)}
                 )
             except BadRequest:
@@ -44,4 +44,4 @@ async def cleanup(c: Smudge, m: Message):
             except Forbidden as e:
                 return await m.reply_text(f"<b>Erro:</b> {e}")
         else:
-            await sent.edit_text(await tld(m, "Admin.cleanup_no_deleted"))
+            return await mes.edit_text(await tld(m, "Admin.cleanup_no_deleted"))
