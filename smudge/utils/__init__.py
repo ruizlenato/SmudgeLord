59a630b621337941ceb981fa0441468637fe748f
RuizLenato <ruizlenato@protonmail.com>
2022-05-22 14:44:24 -0300

    Utils: avoid 'PY-W2000'

--- smudge/utils/__init__.py
+++ smudge/utils/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2021-2022 Luiz Renato (ruizlenato@protonmail.com)
 
+from typing import List
+
 from .utils import (
     http,
     pretty_size,
@@ -8,3 +10,12 @@ from .utils import (
     EMOJI_PATTERN,
     send_logs,
 )
+
+__all__: List[str] = [
+    "http",
+    "pretty_size",
+    "EMOJI_PATTERN",
+    "aiowrap",
+    "button_parser",
+    "send_logs",
+]
