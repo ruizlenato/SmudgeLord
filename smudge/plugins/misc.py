c072126ad103c05208842a65a46e3a8f1ab69e4b
ruizlenato <ruizlenato@protonmail.com>
2022-09-11 15:39:25 -0300

    Plugins: move sed plugin to misc.

--- smudge/plugins/misc.py
+++ smudge/plugins/misc.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2021-2022 Luiz Renato (ruizlenato@proton.me)
+import regex
 import html
 import json
 
@@ -49,6 +50,43 @@ async def translate(c: Smudge, m: Message):
             from_lang=trres.lang, to_lang=to_lang, translation=res
         )
     )
+
+
+@Smudge.on_message(filters.regex(r"^s/(.+)?/(.+)?(/.+)?") & filters.reply)
+async def sed(c: Smudge, m: Message):
+    exp = regex.split(r"(?<![^\\]\\)/", m.text)
+    pattern = exp[1]
+    replace_with = exp[2].replace(r"\/", "/")
+    rflags = 0
+
+    flags = exp[3] if len(exp) > 3 else ""
+    count = 0 if "g" in flags else 1
+    if "i" in flags and "s" in flags:
+        rflags = regex.I | regex.S
+    elif "i" in flags:
+        rflags = regex.I
+    elif "s" in flags:
+        rflags = regex.S
+
+    text = m.reply_to_message.text or m.reply_to_message.caption
+
+    if not text:
+        return
+
+    try:
+        res = regex.sub(
+            pattern, replace_with, text, count=count, flags=rflags, timeout=1
+        )
+    except TimeoutError:
+        await m.reply_text(await tld(m, "Misc.regex_timeout"))
+    except regex.error as e:
+        await m.reply_text(str(e))
+    else:
+        await c.send_message(
+            m.chat.id,
+            f"{html.escape(res)}",
+            reply_to_message_id=m.reply_to_message.id,
+        )
 
 
 @Smudge.on_message(filters.command("dicio"))
