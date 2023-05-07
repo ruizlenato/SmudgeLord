2c7186530e30261fd54dcdc9ad968c72dfb88f40
RuizLenato <ruizlenato@protonmail.com>
2023-05-07 20:37:58 -0300

    Chore: Improvements in the language module

--- smudge/utils/locale.py
+++ smudge/utils/locale.py
@@ -1,22 +1,26 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2023 Luiz Renato (ruizlenato@proton.me)
 import gettext
-from typing import Union
 
-from pyrogram import Client
 from pyrogram.enums import ChatType
-from pyrogram.types import CallbackQuery, Message
+from pyrogram.types import CallbackQuery
 
 from ..database import database
 
 conn = database.get_conn()
 
 
-async def get_db_lang(id: int, type: str):
-    if type == ChatType.PRIVATE:
-        cursor = await conn.execute("SELECT lang FROM users WHERE id = (?)", (id,))
-    elif type in (ChatType.GROUP, ChatType.SUPERGROUP):
-        cursor = await conn.execute("SELECT lang FROM groups WHERE id = (?)", (id,))
+async def get_db_lang(m):
+    if isinstance(m, CallbackQuery):
+        m = m.message
+    if m.chat.type == ChatType.PRIVATE:
+        cursor = await conn.execute(
+            "SELECT lang FROM users WHERE id = (?)", (m.chat.id,)
+        )
+    elif m.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
+        cursor = await conn.execute(
+            "SELECT lang FROM groups WHERE id = (?)", (m.chat.id,)
+        )
     try:
         row = await cursor.fetchone()
         await cursor.close()
@@ -27,16 +31,11 @@ async def get_db_lang(id: int, type: str):
 
 def locale():
     def decorator(func):
-        async def wrapper(
-            client: Client, m: Union[CallbackQuery, Message], *args, **kwargs
-        ):
-            if isinstance(m, CallbackQuery):
-                m = m.message
-
-            lang = await get_db_lang(m.chat.id, m.chat.type)
+        async def wrapper(c, m, *args, **kwargs):
+            lang = await get_db_lang(m)
             load = gettext.translation("bot", "locales", languages=["pt_BR"])
             load.install()
-            return await func(client, m, *args, **kwargs)
+            return await func(c, m, *args, **kwargs)
 
         return wrapper
 
