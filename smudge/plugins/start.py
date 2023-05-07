192a95310fe2a574b6056b33eedaacc6d7ada6c6
RuizLenato <ruizlenato@protonmail.com>
2023-05-07 20:49:36 -0300

    Feat: Updates translations

--- smudge/plugins/start.py
+++ smudge/plugins/start.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2023 Luiz Renato (ruizlenato@proton.me)
+import glob
+import importlib
 from typing import Union
 
 from pyrogram import filters
@@ -9,6 +11,15 @@ from pyrogram.types import CallbackQuery, Message
 
 from ..bot import Smudge
 from ..utils.locale import locale
+
+HELPABLE = {}
+
+mod_paths = glob.glob(f"smudge/plugins/*.py")
+for f in mod_paths:
+    if not f.endswith("__init__.py"):
+        imported_module = importlib.import_module((f)[:-3].replace("/", "."))
+        if hasattr(imported_module, "__help_name__"):
+            HELPABLE[imported_module.__help_name__] = {"Help": imported_module.__help_text__}
 
 
 @Smudge.on_message(filters.command("start"))
@@ -24,8 +35,8 @@ async def start_command(c: Smudge, m: Union[Message, CallbackQuery]):
     if chat_type == ChatType.PRIVATE:
         keyboard = [
             [
-                (_("Language Button"), "setchatlang"),
-                (_("Help Button"), "menu"),
+                (_("🌐Language"), "setchatlang"),
+                (_("❓Help"), "help-menu"),
             ],
             [
                 (
@@ -35,8 +46,43 @@ async def start_command(c: Smudge, m: Union[Message, CallbackQuery]):
                 ),
             ],
         ]
-        text = _("Start Private").format(m.from_user.first_name)
+        text = _("Hello <b>{}</b>, my name is <b>SmudgeLord,</b> I'm a bot with some fun and useful commands for you :3\n\n \
+📦 <b>My SourceCode:</b> <a href='https://github.com/ruizlenato/SmudgeLord'>GitHub</a>\n \
+💬 If you have a <b>problem</b> <a href='https://t.me/RuizLenato'>click here to talk to my developer.</a>"
+        ).format(m.from_user.first_name)
     else:
-        text = _("Start Group")
-
+        text = _("Hello!, I'm SmudgeLord. I have a lot of functions, to know more, start a conversation with me.")
     await reply_text(text, reply_markup=ikb(keyboard), disable_web_page_preview=True)
+
+
+@Smudge.on_callback_query(filters.regex(r"^help-menu"))
+@locale()
+async def help_menu(c: Smudge, m: Union[Message, CallbackQuery]):
+    if isinstance(m, CallbackQuery):
+        reply_text = m.edit_message_text
+    else:
+        reply_text = m.reply_text
+    keyboard = [
+        [
+            (
+                _(help),
+                f"help-plugin {help}",
+            )
+            for help in HELPABLE
+        ],
+    ]
+    await reply_text(
+        _(
+            "Here are all my plugins, to find out more about the plugins, <b>just click on their name.</b>"
+        ),
+        reply_markup=ikb(keyboard),
+    )
+
+
+@Smudge.on_callback_query(filters.regex(pattern="^help-plugin (?P<module>.+)"))
+@locale()
+async def help_plugin(c: Smudge, cq: CallbackQuery):
+    match = cq.matches[0]["module"]
+    keyboard = [[(_("↩️ Back"), "help-menu")]]
+    text = _("<b>Avaliable Commands:</b>\n\n") + HELPABLE[match]["Help"]
+    await cq.edit_message_text(text, reply_markup=ikb(keyboard))
