946f5e45c436620fd4e5943c998ac33ed854d506
ruizlenato <ruizlenato@protonmail.com>
2023-05-17 21:17:20 -0300

    Feat: Adds youtube-dl
    Co-Authored-By: Rian Felipe Celestrino <39718272+RianFC@users.noreply.github.com>

--- smudge/plugins/medias.py
+++ smudge/plugins/medias.py
@@ -1,24 +1,176 @@
+# SPDX-License-Identifier: GPL-3.0
+# Copyright (c) 2023 Luiz Renato (ruizlenato@proton.me)
+import contextlib
+import datetime
 import gettext
+import io
 import re
 
 import filetype
 from pyrogram import filters
 from pyrogram.enums import ChatAction, ChatType
+from pyrogram.errors import BadRequest
+from pyrogram.helpers import ikb
 from pyrogram.raw.functions import channels, messages
 from pyrogram.raw.types import InputMessageID
-from pyrogram.types import InputMediaPhoto, InputMediaVideo, Message
+from pyrogram.types import CallbackQuery, InputMediaPhoto, InputMediaVideo, Message
+from yt_dlp import YoutubeDL
 
 from ..bot import Smudge
 from ..utils.locale import locale
-from ..utils.medias import DownloadMedia
+from ..utils.medias import DownloadMedia, extract_info
+from ..utils.utils import http, pretty_size
 
 REGEX_LINKS = r"(?:htt.+?//)?(?:.+?)?(?:instagram|twitter|tiktok|facebook).com\/(?:\S*)"
+# Regex to get the video ID from the URL
+YOUTUBE_REGEX = re.compile(
+    r"(?m)http(?:s?):\/\/(?:www\.)?(?:music\.)?youtu(?:be\.com\/(watch\?v=|shorts/|embed/)|\.be\/|)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?"
+)
 
 
-@Smudge.on_message(filters.command(["dl", "sdl"]) | filters.regex(REGEX_LINKS), group=1)
+@Smudge.on_message(filters.command("ytdl"))
 @locale()
-async def sdl(client: Smudge, message: Message, _):
-    analise = False
+async def ytdlcmd(client: Smudge, message: Message, _):
+    user = message.from_user.id
+
+    if message.reply_to_message and message.reply_to_message.text:
+        url = message.reply_to_message.text
+    elif len(message.command) > 1:
+        url = message.text.split(None, 1)[1]
+    else:
+        await message.reply_text(
+            _(
+                "<b>Usage:</b> <code>/ytdl [Word or link]</code>\
+\n\nSpecify a word or a link so that I can search and download a video."
+            )
+        )
+        return
+
+    ydl = YoutubeDL({"noplaylist": True})
+    if rege := YOUTUBE_REGEX.match(url):
+        yt = await extract_info(ydl, rege.group(), download=False)
+
+    else:
+        yt = await extract_info(ydl, f"ytsearch:{url}", download=False)
+        try:
+            yt = yt["entries"][0]
+        except IndexError:
+            return
+    for f in yt["formats"]:
+        if f["format_id"] == "140":
+            afsize = f["filesize"] or 0
+        if f["ext"] == "mp4" and f["filesize"] is not None:
+            vfsize = f["filesize"] or 0
+            vformat = f["format_id"]
+    keyboard = [
+        [
+            (
+                _("💿 Audio"),
+                f'_aud.{yt["id"]}|{afsize}|{vformat}|{user}|{message.id}',
+            ),
+            (
+                _("📹 Video"),
+                f'_vid.{yt["id"]}|{vfsize}|{vformat}|{user}|{message.id}',
+            ),
+        ]
+    ]
+
+    if " - " in yt["title"]:
+        performer, title = yt["title"].rsplit(" - ", 1)
+    else:
+        performer = yt.get("creator") or yt.get("uploader")
+        title = yt["title"]
+
+    text = f"🎧 <b>{performer}</b> - <i>{title}</i>\n"
+    text += f"💾 <code>{pretty_size(afsize)}</code> (audio) / "
+    text += f"<code>{pretty_size(int(vfsize))}</code> (video)\n"
+    text += f"⏳ <code>{datetime.timedelta(seconds=yt.get('duration'))}</code>"
+
+    await message.reply_text(text, reply_markup=ikb(keyboard))
+
+
+@Smudge.on_callback_query(filters.regex("^(_(vid|aud))"))
+@locale()
+async def cli_ytdl(client: Smudge, callback: CallbackQuery, _):
+    try:
+        data, fsize, vformat, userid, mid = callback.data.split("|")
+    except ValueError:
+        return print(callback.data)
+    if callback.from_user.id != int(userid):
+        return await callback.answer(_("This button is not for you."), cache_time=60)
+    if int(fsize) > 2147483648:
+        return await callback.answer(
+            _(
+                "The Video you want to download exceeds 2GB in size.\
+\nUnable to download and upload, sorry."
+            ),
+            show_alert=True,
+            cache_time=60,
+        )
+
+    vid = re.sub(r"^\_(vid|aud)\.", "", data)
+    url = f"https://www.youtube.com/watch?v={vid}"
+    await callback.message.edit(_("Downloading..."))
+
+    try:  # Downloader
+        file = io.BytesIO()
+        with contextlib.redirect_stdout(file), YoutubeDL({"outtmpl": "-"}) as ydl:
+            format = f"{vformat}+140" if "vid" in data else "ba[ext=m4a]"
+            ydl.params.update({"format": format, "noplaylist": True})
+            yt = await extract_info(ydl, url, download=True)
+        file.name = yt["title"]
+    except BaseException as e:
+        return await callback.message.edit_text(
+            _(
+                "Sorry! I couldn't send the video because of an error.\
+\n<b>Error:</b> <code>{}</code>"
+            ).format(errmsg=e)
+        )
+    await callback.message.edit(_("Sending..."))
+    await client.send_chat_action(callback.message.chat.id, ChatAction.UPLOAD_VIDEO)
+
+    thumb = io.BytesIO((await http.get(yt["thumbnail"])).content)
+    thumb.name = "thumbnail.png"
+    caption = f"<a href='{yt['webpage_url']}'>{yt['title']}</a></b>"
+
+    try:
+        if "vid" in data:
+            await client.send_video(
+                callback.message.chat.id,
+                video=file,
+                width=1920,
+                height=1080,
+                caption=caption,
+                duration=yt["duration"],
+                thumb=thumb,
+                reply_to_message_id=int(mid),
+            )
+        else:
+            if " - " in yt["title"]:
+                performer, title = yt["title"].rsplit(" - ", 1)
+            else:
+                performer = yt.get("creator") or yt.get("uploader")
+                title = yt["title"]
+                await client.send_audio(
+                    callback.message.chat.id,
+                    audio=file,
+                    title=title,
+                    performer=performer,
+                    caption=caption,
+                    duration=yt["duration"],
+                    thumb=thumb,
+                    reply_to_message_id=int(mid),
+                )
+    except BadRequest as e:
+        await callback.message.edit_text(
+            _(
+                "Sorry! I couldn't send the video because of an error.\
+\n<b>Error:</b> <code>{}</code>"
+            ).format(errmsg=e)
+        )
+    await callback.message.delete()
+    return None
+
     if message.matches:
         if message.chat.type is ChatType.PRIVATE or analise:
             url = message.matches[0].group(0)
--- smudge/utils/utils.py
+++ smudge/utils/utils.py
@@ -1,4 +1,5 @@
 import asyncio
+import math
 from collections.abc import Callable
 from functools import partial, wraps
 
@@ -17,3 +18,13 @@ def aiowrap(func: Callable) -> Callable:
         return await loop.run_in_executor(executor, pfunc)
 
     return run
+
+
+def pretty_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return f"{s} {size_name[i]}"
