1c1cf12ae41c2c1aa42a4d94e6d42a6a7b9d9662
RuizLenato <ruizlenato@proton.me>
2023-07-26 20:56:56 -0300

    Chore: Updates instagram scrapper

--- smudge/utils/medias.py
+++ smudge/utils/medias.py
@@ -165,8 +165,8 @@ class DownloadMedia:
             else:
                 return
 
-            if "video" in data:
-                video = data["videp"]
+            if "video" in data[0]:
+                video = data[0]["video"]
                 if len(video) == 1:
                     await self.downloader(
                         video[0]["contentUrl"], int(video[0]["width"]), int(video[0]["height"])
