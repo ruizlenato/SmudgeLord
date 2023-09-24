74fdc5253be6b569e2d53129f30cc5eeb9ea017b
RuizLenato <ruizlenato@proton.me>
2023-09-24 16:06:17 -0300

    Chore: Adds integration with Sentry.io

--- smudge/bot.py
+++ smudge/bot.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2023 Luiz Renato (ruizlenato@proton.me)
+import sentry_sdk
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 
@@ -7,6 +8,7 @@ from smudge.database import database
 from smudge.utils.utils import http
 
 from .config import config
+from .utils.logger import log
 
 
 class Smudge(Client):
@@ -25,6 +27,9 @@ class Smudge(Client):
         )
 
     async def start(self):
+        if SENTRY_KEY := config["SENTRY_KEY"]:
+            log.info("\033[94m[📉] - Starting Sentry integration...\033[0m")
+            sentry_sdk.init(SENTRY_KEY)
         await database.connect()
         await super().start()
 
