14ccf278ceb0be606460b521a65f01c5ef5de95a
RuizLenato <ruizlenato@proton.me>
2024-01-16 01:48:41 -0300

    refactor: Improve code formatting and readability with black

--- smudge/plugins/start.py
+++ smudge/plugins/start.py
@@ -61,7 +61,11 @@ async def start_command(client: Smudge, union: Message | CallbackQuery, strings)
 @Smudge.on_message(filters.command("help") & filters.private)
 @locale("start")
 async def help_menu(client: Smudge, union: Message | CallbackQuery, strings):
-    reply_text = union.edit_message_text if isinstance(union, CallbackQuery) else union.reply_text
+    reply_text = (
+        union.edit_message_text
+        if isinstance(union, CallbackQuery)
+        else union.reply_text
+    )
     buttons: list = []
     for help in sorted(HELPABLE):
         buttons.append((await get_string(union, help, "name"), f"help-plugin {help}"))
@@ -92,7 +96,11 @@ async def help_plugin(client: Smudge, callback: CallbackQuery, strings):
 @Smudge.on_message(filters.command("config"))
 @locale("config")
 async def config(client: Smudge, union: Message | CallbackQuery, strings):
-    reply = union.edit_message_text if isinstance(union, CallbackQuery) else union.reply_text
+    reply = (
+        union.edit_message_text
+        if isinstance(union, CallbackQuery)
+        else union.reply_text
+    )
 
     if not await filters.admin(client, union):
         if isinstance(union, CallbackQuery):
@@ -127,4 +135,6 @@ async def about_menu(client: Smudge, union: Message | CallbackQuery, strings):
         ],
     ]
     text = strings["about-text"].format(__version__, __commit__)
-    await union.edit_message_text(text, reply_markup=ikb(keyboard), disable_web_page_preview=True)
+    await union.edit_message_text(
+        text, reply_markup=ikb(keyboard), disable_web_page_preview=True
+    )
