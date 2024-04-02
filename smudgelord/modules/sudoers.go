d1d2897bc713b0168025f4346e04d5d4cb864690
RuizLenato <ruizlenato@proton.me>
2024-04-02 12:06:47 -0300

    chore: improve announce message handling and localization

--- smudgelord/modules/sudoers.go
+++ smudgelord/modules/sudoers.go
@@ -2,38 +2,75 @@ package modules
 
 import (
 	"fmt"
+	"log"
 	"strings"
 
 	"smudgelord/smudgelord/config"
 	"smudgelord/smudgelord/database"
+	"smudgelord/smudgelord/localization"
 
 	"github.com/mymmrac/telego"
 	"github.com/mymmrac/telego/telegohandler"
 	"github.com/mymmrac/telego/telegoutil"
 )
 
-func announce(bot *telego.Bot, message telego.Message) {
-	if message.From.ID != config.OWNER_ID {
+var announceMessageText string // Variável global para armazenar o texto da mensagem
+
+func announce(bot *telego.Bot, update telego.Update) {
+	var lang string
+	message := update.Message
+
+	if message == nil {
+		message = update.CallbackQuery.Message.(*telego.Message)
+		lang = strings.ReplaceAll(update.CallbackQuery.Data, "announce ", "")
+	}
+
+	if (message == nil || message.From.ID != config.OWNER_ID) &&
+		(update.CallbackQuery == nil || update.CallbackQuery.From.ID != config.OWNER_ID) {
 		return
 	}
-	messageFields := strings.Fields(message.Text)
+
+	if lang == "" {
+		buttons := make([][]telego.InlineKeyboardButton, 0, len(database.AvailableLocales))
+		for _, lang := range database.AvailableLocales {
+			loaded, ok := localization.LangCache[lang]
+			if !ok {
+				log.Fatalf("Language '%s' not found in the cache.", lang)
+			}
+
+			buttons = append(buttons, []telego.InlineKeyboardButton{{
+				Text: localization.GetStringFromNestedMap(loaded, "language.flag") +
+					localization.GetStringFromNestedMap(loaded, "language.name"),
+				CallbackData: fmt.Sprintf("announce %s", lang),
+			}})
+		}
+
+		bot.SendMessage(&telego.SendMessageParams{
+			ChatID:      telegoutil.ID(config.OWNER_ID),
+			Text:        "Choose a language:",
+			ParseMode:   "HTML",
+			ReplyMarkup: telegoutil.InlineKeyboard(buttons...),
+		})
+		announceMessageText = message.Text
+		return
+	}
+
+	messageFields := strings.Fields(announceMessageText)
 	if len(messageFields) < 2 {
 		return
 	}
 
 	announceType := messageFields[1]
-	announceText := strings.Join(strings.Fields(message.Text)[2:], " ")
+	announceText := strings.Join(messageFields[2:], " ")
 	var query string
-
-	fmt.Println(announceType)
 
 	switch announceType {
 	case "groups":
-		query = "SELECT id FROM groups;"
+		query = fmt.Sprintf("SELECT id FROM groups WHERE language = '%s';", lang)
 	case "users":
-		query = "SELECT id FROM users;"
+		query = fmt.Sprintf("SELECT id FROM users WHERE language = '%s';", lang)
 	default:
-		query = "SELECT id FROM users UNION ALL SELECT id FROM groups;"
+		query = fmt.Sprintf("SELECT id FROM users WHERE language = '%s' UNION ALL SELECT id FROM groups WHERE language = '%s';", lang, lang)
 		announceText = strings.Join(messageFields[1:], " ")
 		if len(messageFields) > 2 {
 			announceText = strings.Join(messageFields[2:], " ")
@@ -71,13 +108,16 @@ func announce(bot *telego.Bot, message telego.Message) {
 		return
 	}
 
-	bot.SendMessage(&telego.SendMessageParams{
+	bot.EditMessageText(&telego.EditMessageTextParams{
 		ChatID:    telegoutil.ID(config.OWNER_ID),
+		MessageID: update.CallbackQuery.Message.GetMessageID(),
 		Text:      fmt.Sprintf("<b>Messages sent successfully:</b> <code>%d</code>\n<b>Messages unsent:</b> <code>%d</code>", successCount, errorCount),
 		ParseMode: "HTML",
 	})
+	announceMessageText = ""
 }
 
 func LoadSudoers(bh *telegohandler.BotHandler, bot *telego.Bot) {
-	bh.HandleMessage(announce, telegohandler.CommandEqual("announce"))
+	bh.Handle(announce, telegohandler.CommandEqual("announce"))
+	bh.Handle(announce, telegohandler.CallbackDataPrefix("announce"))
 }
