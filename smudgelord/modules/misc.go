a2c5a0f766aeea583db0891ee384e3194537c75d
RuizLenato <ruizlenato@proton.me>
2024-04-17 01:30:03 -0300

    feat(misc): add translate plugin

--- smudgelord/modules/misc.go
+++ smudgelord/modules/misc.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
+	"net/url"
 	"strings"
 
 	"smudgelord/smudgelord/localization"
@@ -93,8 +95,152 @@ func weather(bot *telego.Bot, message telego.Message) {
 	})
 }
 
+func getTrLang(text string, chat telego.Chat) string {
+	languages := [135]string{
+		`af`, `sq`, `am`, `ar`, `hy`,
+		`as`, `ay`, `az`, `bm`, `eu`,
+		`be`, `bn`, `bho`, `bs`, `bg`,
+		`ca`, `ceb`, `zh`, `co`, `hr`,
+		`cs`, `da`, `dv`, `doi`, `nl`,
+		`en`, `eo`, `et`, `ee`, `fil`,
+		`fi`, `fr`, `fy`, `gl`, `ka`,
+		`de`, `el`, `gn`, `gu`, `ht`,
+		`ha`, `haw`, `he`, `iw`, `hi`,
+		`hmn`, `hu`, `is`, `ig`, `ilo`,
+		`id`, `ga`, `it`, `ja`, `jv`,
+		`jw`, `kn`, `kk`, `km`, `rw`,
+		`gom`, `ko`, `kri`, `ku`, `ckb`,
+		`ky`, `lo`, `la`, `lv`, `ln`,
+		`lt`, `lg`, `lb`, `mk`, `mai`,
+		`mg`, `ms`, `ml`, `mt`, `mi`,
+		`mr`, `mni`, `lus`, `mn`, `my`,
+		`ne`, `no`, `ny`, `or`, `om`,
+		`ps`, `fa`, `pl`, `pt`, `pa`,
+		`qu`, `ro`, `ru`, `sm`, `sa`,
+		`gd`, `nso`, `sr`, `st`, `sn`,
+		`sd`, `si`, `sk`, `sl`, `so`,
+		`es`, `su`, `sw`, `sv`, `tl`,
+		`tg`, `ta`, `tt`, `te`, `th`,
+		`ti`, `ts`, `tr`, `tk`, `ak`,
+		`uk`, `ur`, `ug`, `uz`, `vi`,
+		`cy`, `xh`, `yi`, `yo`, `zu`,
+	}
+	checkLang := func(item string) bool {
+		for _, s := range languages {
+			if s == item {
+				return true
+			}
+		}
+		return false
+	}
+
+	chatLang, err := localization.GetChatLanguage(chat)
+	if err != nil {
+		chatLang = "en"
+	}
+
+	if len(strings.Fields(text)) > 0 {
+		lang := strings.Fields(text)[0]
+		langParts := strings.Split(lang, "-")
+
+		if !checkLang(langParts[0]) {
+			lang = strings.Split(chatLang, "-")[0]
+		}
+
+		if len(langParts) > 1 && !checkLang(langParts[1]) {
+			lang = strings.Split(chatLang, "-")[0]
+		}
+
+		return lang
+	}
+	return "en"
+}
+
+func gTranslate(bot *telego.Bot, message telego.Message) {
+	text := message.Text[4:]
+	if messageFields := strings.Fields(message.Text); messageFields[0] == "/translate" {
+		text = message.Text[11:]
+	}
+	var sourceLang string
+	var targetLang string
+
+	language := getTrLang(text, message.Chat)
+	if strings.HasPrefix(text, language) {
+		text = strings.Replace(text, language, "", 1)
+		text = strings.TrimSpace(text)
+	}
+
+	if langParts := strings.Split(language, "-"); len(langParts) > 1 {
+		sourceLang = langParts[0]
+		targetLang = langParts[1]
+	} else {
+		targetLang = language
+		sourceLang = "auto"
+	}
+
+	translation := new(struct {
+		Sentences []struct {
+			Trans   string `json:"trans"`
+			Orig    string `json:"orig"`
+			Backend int    `json:"backend"`
+		} `json:"sentences"`
+		Source string `json:"src"`
+	})
+
+	devices := []string{
+		"Linux; U; Android 10; Pixel 4",
+		"Linux; U; Android 10; Pixel 4 XL",
+		"Linux; U; Android 10; Pixel 4a",
+		"Linux; U; Android 10; Pixel 4a XL",
+		"Linux; U; Android 11; Pixel 4",
+		"Linux; U; Android 11; Pixel 4 XL",
+		"Linux; U; Android 11; Pixel 4a",
+		"Linux; U; Android 11; Pixel 4a XL",
+		"Linux; U; Android 11; Pixel 5",
+		"Linux; U; Android 11; Pixel 5a",
+		"Linux; U; Android 12; Pixel 4",
+		"Linux; U; Android 12; Pixel 4 XL",
+		"Linux; U; Android 12; Pixel 4a",
+		"Linux; U; Android 12; Pixel 4a XL",
+		"Linux; U; Android 12; Pixel 5",
+		"Linux; U; Android 12; Pixel 5a",
+		"Linux; U; Android 12; Pixel 6",
+		"Linux; U; Android 12; Pixel 6 Pro",
+	}
+
+	headers := map[string]string{
+		`User-Agent`:   fmt.Sprintf(`GoogleTranslate/6.28.0.05.421483610 (%s)`, devices[rand.Intn(len(devices))]),
+		`Content-Type`: `application/x-www-form-urlencoded;charset=utf-8`,
+	}
+
+	body := utils.RequestPOST(fmt.Sprintf("https://translate.google.com/translate_a/single?client=at&dt=t&dj=1&sl=%s&tl=%s&q=%s",
+		sourceLang, targetLang, url.QueryEscape(text)),
+		utils.RequestPOSTParams{Headers: headers}).Body()
+
+	err := json.Unmarshal(body, &translation)
+	if err != nil {
+		fmt.Println("[Misc/gTranslate] Error unmarshalling translation data:", err)
+	}
+
+	var translations []string
+	for _, sentence := range translation.Sentences {
+		translations = append(translations, sentence.Trans)
+	}
+	textUnescaped, _ := (url.QueryUnescape(strings.Join(translations, "")))
+
+	bot.SendMessage(&telego.SendMessageParams{
+		ChatID:    telegoutil.ID(message.From.ID),
+		Text:      fmt.Sprintf("<b>%s</b> -> <b>%s</b>\n<code>%s</code>", translation.Source, targetLang, textUnescaped),
+		ParseMode: "HTML",
+	})
+}
+
 func LoadMisc(bh *telegohandler.BotHandler, bot *telego.Bot) {
 	helpers.Store("misc")
 	bh.HandleMessage(weather, telegohandler.CommandEqual("weather"))
 	bh.HandleMessage(weather, telegohandler.CommandEqual("clima"))
+	bh.HandleMessage(gTranslate, telegohandler.Or(
+		telegohandler.CommandEqual("translate"),
+		telegohandler.CommandEqual("tr")),
+	)
 }
