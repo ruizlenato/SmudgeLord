d222f34477ba33a2ec087bfbe87a8549b726f422
RuizLenato <ruizlenato@proton.me>
2024-06-09 15:23:28 -0300

    feat: Enhance TikTok downloader to use direct API and data structures
    Refactored TikTok downloader to use a direct API call and improved error handling. Added support for various media types and updated data structures to include detailed video, image, and author information.

--- smudgelord/modules/medias/downloader/tiktok/tiktok.go
+++ smudgelord/modules/medias/downloader/tiktok/tiktok.go
@@ -2,8 +2,12 @@ package tiktok
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"regexp"
+	"slices"
 	"sync"
 
 	"smudgelord/smudgelord/modules/medias/downloader"
@@ -17,31 +21,67 @@ func TikTok(url string) ([]telego.InputMedia, string) {
 	var mediaItems []telego.InputMedia
 	var caption string
 
-	body := utils.RequestGET("https://scrapper.ruizlenato.workers.dev/", utils.RequestGETParams{Query: map[string]string{"url": url}}).Body()
+	res, err := http.Get(url)
+	if err != nil {
+		log.Printf("[tiktok/TikTok] Error getting TikTok URL: %v", err)
+		return nil, caption
+	}
+	defer res.Body.Close()
+
+	matches := regexp.MustCompile(`/(?:video|photo|v)/(\d+)`).FindStringSubmatch(res.Request.URL.String())
+	if len(matches) != 2 {
+		log.Printf("[tiktok/TikTok] No video ID found in URL: %s", res.Request.URL.String())
+		return nil, caption
+	}
+	videoID := matches[1]
+
+	body := utils.Request("https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/", utils.RequestParams{
+		Method: "OPTIONS",
+		Headers: map[string]string{
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0",
+		},
+		Query: map[string]string{
+			"iid":             "7318518857994389254",
+			"device_id":       "7318517321748022790",
+			"channel":         "googleplay",
+			"version_code":    "300904",
+			"device_platform": "android",
+			"device_type":     "ASUS_Z01QD",
+			"os_version":      "9",
+			"aweme_id":        videoID,
+			"aid":             "1128",
+		},
+	}).Body()
+
 	if body == nil {
+		log.Printf("[tiktok/TikTok] No response body for video ID: %s", videoID)
 		return nil, caption
 	}
 
 	var tikTokData TikTokData
-	err := json.Unmarshal(body, &tikTokData)
+	err = json.Unmarshal(body, &tikTokData)
 	if err != nil {
 		log.Printf("[tiktok/TikTok] Error unmarshalling TikTok data: %v", err)
 		return nil, caption
 	}
 
-	if tikTokData == nil {
+	if len(tikTokData.AwemeList) == 0 {
 		return nil, caption
 	}
 
-	if tikTokData.Images != nil {
+	if tikTokData.AwemeList[0].Author.Nickname != nil && tikTokData.AwemeList[0].Desc != nil {
+		caption = fmt.Sprintf("<b>%s</b>:\n%s", *tikTokData.AwemeList[0].Author.Nickname, *tikTokData.AwemeList[0].Desc)
+	}
+
+	if slices.Contains([]int{2, 68, 150}, tikTokData.AwemeList[0].AwemeType) {
 		var wg sync.WaitGroup
-		wg.Add(len(*tikTokData.Images))
+		wg.Add(len(tikTokData.AwemeList[0].ImagePostInfo.Images))
 
 		medias := make(map[int]*os.File)
-		for i, media := range *tikTokData.Images {
-			go func(index int, media string) {
+		for i, media := range tikTokData.AwemeList[0].ImagePostInfo.Images {
+			go func(index int, media Image) {
 				defer wg.Done()
-				file, err := downloader.Downloader(media)
+				file, err := downloader.Downloader(media.DisplayImage.URLList[1])
 				if err != nil {
 					log.Print("[tiktok/TikTok] Error downloading photo:", err)
 					// Use index as key to store nil for failed downloads
@@ -63,16 +103,15 @@ func TikTok(url string) ([]telego.InputMedia, string) {
 			}
 		}
 	} else {
-		file, err := downloader.Downloader(tikTokData.VideoURL)
+		file, err := downloader.Downloader(tikTokData.AwemeList[0].Video.PlayAddr.URLList[0])
 		if err != nil {
 			log.Print("[tiktok/TikTok] Error downloading video:", err)
 			return nil, caption
 		}
 		mediaItems = append(mediaItems, telegoutil.MediaVideo(
-			telegoutil.File(file)),
+			telegoutil.File(file)).WithWidth(tikTokData.AwemeList[0].Video.PlayAddr.Width).WithHeight(tikTokData.AwemeList[0].Video.PlayAddr.Height),
 		)
 	}
-	caption = tikTokData.Title
 
 	return mediaItems, caption
 }
