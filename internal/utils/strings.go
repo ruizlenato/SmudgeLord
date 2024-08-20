92feffb1c1548703824bb357545d97d39b770127
RuizLenato <ruizlenato@proton.me>
2024-08-20 20:35:49 -0300

    refactor(utils): update text formatting function to support UTF-16 encoding
    - Modified the `FormatText` function in `strings.go` to use UTF-16 encoding for text manipulation.
    - Updated the code to encode and decode text runes using the `utf16` package.
    - This change ensures proper handling of special characters and improves compatibility with messaging platforms.

--- internal/utils/strings.go
+++ internal/utils/strings.go
@@ -2,31 +2,31 @@ package utils
 
 import (
 	"fmt"
+	"unicode/utf16"
 
 	"github.com/mymmrac/telego"
 )
 
 func FormatText(text string, entities []telego.MessageEntity) string {
-	// Convert text to a slice of runes
-	textRunes := []rune(text)
+	textRunes := utf16.Encode([]rune(text))
 
-	// Insert entities in reverse order to keep track of shifting offsets
 	for i := len(entities) - 1; i >= 0; i-- {
 		entity := entities[i]
 		switch entity.Type {
 		case "bold":
-			textRunes = append(textRunes[:entity.Offset+entity.Length], append([]rune("</b>"), textRunes[entity.Offset+entity.Length:]...)...)
-			textRunes = append(textRunes[:entity.Offset], append([]rune("<b>"), textRunes[entity.Offset:]...)...)
+			textRunes = append(textRunes[:entity.Offset+entity.Length], append(utf16.Encode([]rune("</b>")), textRunes[entity.Offset+entity.Length:]...)...)
+			textRunes = append(textRunes[:entity.Offset], append(utf16.Encode([]rune("<b>")), textRunes[entity.Offset:]...)...)
 		case "italic":
-			textRunes = append(textRunes[:entity.Offset+entity.Length], append([]rune("</i>"), textRunes[entity.Offset+entity.Length:]...)...)
-			textRunes = append(textRunes[:entity.Offset], append([]rune("<i>"), textRunes[entity.Offset:]...)...)
+			textRunes = append(textRunes[:entity.Offset+entity.Length], append(utf16.Encode([]rune("</i>")), textRunes[entity.Offset+entity.Length:]...)...)
+			textRunes = append(textRunes[:entity.Offset], append(utf16.Encode([]rune("<i>")), textRunes[entity.Offset:]...)...)
 		case "code":
-			textRunes = append(textRunes[:entity.Offset+entity.Length], append([]rune("</code>"), textRunes[entity.Offset+entity.Length:]...)...)
-			textRunes = append(textRunes[:entity.Offset], append([]rune("<code>"), textRunes[entity.Offset:]...)...)
+			textRunes = append(textRunes[:entity.Offset+entity.Length], append(utf16.Encode([]rune("</code>")), textRunes[entity.Offset+entity.Length:]...)...)
+			textRunes = append(textRunes[:entity.Offset], append(utf16.Encode([]rune("<code>")), textRunes[entity.Offset:]...)...)
 		case "text_link":
-			textRunes = append(textRunes[:entity.Offset+entity.Length], append([]rune("</a>"), textRunes[entity.Offset+entity.Length-1:]...)...)
-			textRunes = append(textRunes[:entity.Offset], append([]rune(fmt.Sprintf("<a href='%s'>", entity.URL)), textRunes[entity.Offset:]...)...)
+			textRunes = append(textRunes[:entity.Offset+entity.Length], append(utf16.Encode([]rune("</a>")), textRunes[entity.Offset+entity.Length:]...)...)
+			textRunes = append(textRunes[:entity.Offset], append(utf16.Encode([]rune(fmt.Sprintf("<a href='%s'>", entity.URL))), textRunes[entity.Offset:]...)...)
 		}
 	}
-	return string(textRunes)
+
+	return string(utf16.Decode(textRunes))
 }
